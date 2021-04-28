731e93196bedc9e5f6df0e8b2525384366676052
MillionerOleg <53629744+MillionerOleg@users.noreply.github.com>
2021-04-28 19:12:29 +0300

    Add files via upload

--- d2-9.py
+++ d2-9.py
@@ -3,7 +3,6 @@ import os
 from bottle import HTTPResponse, Bottle, request
 from sentry_sdk.integrations.bottle import BottleIntegration
 
-# Можете проверить, вставив свой DSN, с другой стороны.. Зачем?
 sentry_dsn = ""
 sentry_sdk.init(
     dsn = sentry_dsn,
@@ -13,18 +12,11 @@ sentry_sdk.init(
 app = Bottle()
 @app.route('/success')  
 def index():  
-    raise HTTPResponse(status=200)
     return
 
 @app.route('/fail')  
 def index2():   
     raise RuntimeError("ошибка сервера")
-    return
-  
-@app.route('/')  
-def index3():  
-    raise HTTPResponse(status=200)
-    return
 
 if os.environ.get("APP_LOCATION") == "heroku":
     app.run(
